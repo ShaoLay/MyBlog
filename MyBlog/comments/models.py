3eb938dc535dbb692dbf957fff1c4db29c9e4212
JavsShao <javs_shao@163.com>
2019-06-11 17:00:06 +0800

    修改主题，完成主要页面

---
+++ MyBlog/comments/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+
+from  blog.models import Article
+
+
+# Create your models here.
+
+class Comment(models.Model):
+    body = models.TextField('正文')
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    last_mod_time = models.DateTimeField('修改时间', auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='作者', on_delete=models.CASCADE)
+
+    article = models.ForeignKey(Article, verbose_name='文章', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created_time']
+        verbose_name = "评论"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.body
